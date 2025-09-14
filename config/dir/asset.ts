5192075a4c43186f4d538629f8dbb5dfbcc54f2e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-14 22:20:14 +1000

    [config-dir]: added asset dir

--- config/dir/asset.ts
+++ config/dir/asset.ts
@@ -1,1 +1,6 @@
-export const ASSET_DIR = {}
+import { combine } from "lib/util"
+
+export const ASSET_DIR = {
+    BACKGROUND_WIDE: combine('asset', 'image', 'wide.jpg'),
+    BACKGROUND_BRIS: combine('asset', 'image', 'brisbane.jpg'),
+}
