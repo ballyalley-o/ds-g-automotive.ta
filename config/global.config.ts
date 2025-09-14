38b39d47fd34534e00103397620e5df9ca261c73
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 02:22:52 +1000

    [global]: vars added

--- config/global.config.ts
+++ config/global.config.ts
@@ -1,1 +1,5 @@
-export const GLOBAL = {}
+export const GLOBAL = {
+    APP_NAME  : process.env.NEXT_PUBLIC_APP_NAME || 'DS Tech Assessment Project',
+    APP_DESC  : process.env.NEXT_PUBLIC_APP_DESC || '',
+    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+}
