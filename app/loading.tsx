75f4886d5a04d9c5bdbaa3c1726745034275e417
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 13:12:49 +1000

    [enhance]

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { formatText } from 'lib/util/format-text'
 
 const Loading = () => {
   return (
-    <div className={'mx-auto max-w-7xl px-lg py-xl animate-[fade-in_.25s_ease]'}>
+    <div className={'m-auto max-w-7xl px-lg py-xl animate-[fade-in_.25s_ease]'}>
       <div className={'flex flex-wrap items-center justify-center gap-lg'}>
-        <h1 className={'text-4xl text-[color:var(--color-palette-gray-300))] font-bold'}>{'Loading...'}</h1>
+        <h1 className={'text-4xl text-[color:var(--color-palette-gray-300))] font-bold'}>{formatText('loading...', 'capitalize')}</h1>
       </div>
     </div>
   )
