ce86f23df1b932829666d112225dda7946349aac
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 11:37:52 +1000

    [refactor]: refactor listing card

--- component/card/variant/listing-card.tsx
+++ component/card/variant/listing-card.tsx
@@ -27,21 +27,25 @@ export type ListingCardProps = {
 
 const ListingCard: React.FC<ListingCardProps> = ({ className, surface = 1, title, description, image, cta, active }) => {
   return (
-    <Card
-      surface={surface}
-      className={cn('w-[300px] h-[length:var(--card-height)] flex flex-col transition-colors duration-200',
-        active && 'border-[color:var(--color-palette-brand)] shadow-[var(--shadow-md)] ring-2 ring-[color:var(--color-palette-brand)]',
-        className
-      )}
-    >
-      <CardImage>
-        <Image src={image.src} alt={image.alt ?? 'card-image'} width={image.width ?? 300} height={image.height ?? 150} priority={image.priority} />
-      </CardImage>
-      <CardBody className={"flex flex-col flex-1"}>
-        <CardHeader>{title}</CardHeader>
-        <p className={"text-justify whitespace-pre-wrap"}>{description}</p>
-        {cta && <Button className={"mt-auto w-full"} variant={cta.variant} disabled={cta.disabled} onClick={cta.onClick} label={cta.label} />}
-      </CardBody>
+    <Card surface={surface} className={cn('relative group w-[300px] h-[length:var(--card-height)] flex flex-col transition-colors duration-200', active && 'border-[color:var(--color-palette-brand)] shadow-[var(--shadow-md)] ring-2 ring-[color:var(--color-palette-brand)]', className)}>
+        <CardImage className="relative flex-shrink-0">
+          <Image src={image.src} alt={image.alt ?? 'card-image'} width={image.width ?? 300} height={image.height ?? 150} priority={image.priority} className={"object-cover w-full h-auto"} />
+        </CardImage>
+          {cta && (
+            <div className={'pointer-events-none absolute inset-0 show-on-hover items-center justify-center opacity-0 transition-opacity duration-200 group-hover:opacity-100 bg-gradient-to-b from-[color:var(--color-palette-darker-blue)]/40 to-[color:var(--color-palette-dark-blue)]/20 p-[length:var(--space-lg)]'}>
+              <div className={"pointer-events-auto"}>
+                <Button variant={cta.variant} disabled={cta.disabled} onClick={cta.onClick} label={cta.label} />
+              </div>
+            </div>
+          )}
+        <CardBody className={"flex flex-col min-h-0 flex-1"}>
+          <CardHeader>{title}</CardHeader>
+          <p className={"text-justify text-sm md:text-md whitespace-pre-wrap line-clamp-6"}>{description}</p>
+
+          {cta && (
+            <Button className={"mt-auto w-full hide-on-hover"} variant={cta.variant} disabled={cta.disabled} onClick={cta.onClick} label={cta.label} />
+          )}
+        </CardBody>
     </Card>
   )
 }
