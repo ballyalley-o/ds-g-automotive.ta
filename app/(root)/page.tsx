b45b95e565d189741df21341d64866a10c2a5e7e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 11:42:01 +1000

    [cleanup]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import React from 'react'
+import { HomeTitle, HomeCover, HomeCardContainer } from 'component/home'
+import { formatText } from 'lib/util/format-text'
 
-export default function Home() {
-  return (<h1> Car Dealer </h1>);
+
+export default function Homepage() {
+  return (
+    <React.Fragment>
+      <section className={'w-full'}>
+        <HomeCover />
+        <div className={'flex flex-wrap justify-center gap-lg px-lg py-xl pb-[length:calc(var(--space-xl)*2)] md:pb-[length:var(--space-xl)] md:h-[length:var(--home-cards-height)]'}>
+          <HomeTitle title={formatText('technical assessment', 'capitalize')} />
+          <HomeCardContainer />
+        </div>
+      </section>
+    </React.Fragment>
+  )
 }
