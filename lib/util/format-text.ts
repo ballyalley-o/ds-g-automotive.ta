4581df2b79802ca5062ea47a8700e5a19121fc4d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 11:43:33 +1000

    [doc]: documentation

--- lib/util/format-text.ts
+++ lib/util/format-text.ts
@@ -1,5 +1,17 @@
 type FormatTextType = 'uppercase' | 'lowercase' | 'capitalize'
 
+/**
+ * Format texts based on the type provided
+ *
+ * @param {string} text - text to be formatted
+ * @param {FormatTextType} type - The type of formatting to apply
+ *                        Accepted values are:
+ *                        - 'capitalize': Capitalizes the first letter and converts the rest to lowercase.
+ *                        - 'uppercase': Converts the entire text to uppercase.
+ *                        - 'lowercase': Converts the entire text to lowercase.
+ *                        - Any other value will return the text unchanged.
+ * @returns {string} - The formatted text
+ */
 export const formatText = (text: string, type: FormatTextType): string => {
    switch (type) {
         case 'uppercase':
