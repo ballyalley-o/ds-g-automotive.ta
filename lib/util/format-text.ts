ef9a69bf234f74bb5c354c1f360ccfdc986b80c8
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-15 11:44:01 +1000

    [refactor]: added exemptions for words

--- lib/util/format-text.ts
+++ lib/util/format-text.ts
@@ -13,13 +13,14 @@ type FormatTextType = 'uppercase' | 'lowercase' | 'capitalize'
  * @returns {string} - The formatted text
  */
 export const formatText = (text: string, type: FormatTextType): string => {
+    const exemptedWords = ['to']
    switch (type) {
         case 'uppercase':
             return text.toUpperCase()
         case 'lowercase':
             return text.toLowerCase()
         case 'capitalize':
-            return text.toLowerCase().split(" ").map(_word => _word.charAt(0).toUpperCase() + _word.slice(1)).join(" ")
+            return text.toLowerCase().split(" ").map(_word => !exemptedWords.includes(_word) ? _word.charAt(0).toUpperCase() + _word.slice(1) : _word).join(" ")
         default:
             return text
    }
